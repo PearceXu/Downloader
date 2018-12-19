d1f32f5fa3ab760cd52ca5c35d9381f6c54b6ac2
xubaipei <847923861@qq.com>
2018-12-19 22:01:44 +0800

    修改MD

--- README.md
+++ README.md
@@ -6,10 +6,9 @@ Downloader in java 多线程下载器，支持断点下载，进度回传
     implementation 'com.github.PearceXu:Downloader:1.0.7'
 
 # 用法
- String dest = "C:\\Users\\xubp\\Desktop\\video";
- String url = "https://c4b130d63ab50d8b92f23a952cc18836.dd.cdntips.com/download.sj.qq.com/upload/connAssitantDownload/upload/MobileAssistant_1.apk";
- url = "http://10.98.0.232:9004/AppUpdate/H5File/20181211/web.zip";
- Downloader downloader = new Downloader(url,dest,1);
+String dest = "C:\\Users\\xubp\\Desktop\\video";
+String url = "http://10.98.0.232:9004/AppUpdate/H5File/20181211/web.zip";
+Downloader downloader = new Downloader(url,dest,1);
  downloader.download(new Downloader.CallBack() {
      @Override
      public void onProgress(int progress) {
--- downloader/src/test/java/my/xubaipei/downloader/ExampleUnitTest.java
+++ downloader/src/test/java/my/xubaipei/downloader/ExampleUnitTest.java
@@ -13,27 +13,5 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception{
         assertEquals(4, 2 + 2);
-        String dest = "C:\\Users\\xubp\\Desktop\\video";
-        String url = "https://c4b130d63ab50d8b92f23a952cc18836.dd.cdntips.com/download.sj.qq.com/upload/connAssitantDownload/upload/MobileAssistant_1.apk";
-        url = "http://10.98.0.232:9004/AppUpdate/H5File/20181211/web.zip";
-        Downloader downloader = new Downloader(url,dest,1);
-        downloader.download(new Downloader.CallBack() {
-            @Override
-            public void onProgress(int progress) {
-                System.out.println(progress);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                e.printStackTrace();
-                System.out.println(e.toString());
-            }
-
-            @Override
-            public void onFinish(String path) {
-                System.out.println(path);
-            }
-        });
-        Thread.sleep(1000 * 60 * 60);
     }
 }
